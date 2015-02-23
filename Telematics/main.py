b5b67ede19b7fdba7059a7302f5ab8cea15319e4
evancchow <echow@princeton.edu>
2015-02-22 23:39:22 -0500

    added angle turning metrics, basic

--- Telematics/main.py
+++ Telematics/main.py
@@ -3,13 +3,15 @@ import numpy as np
 from numpy.linalg import norm
 import os
 from os import listdir
-import random
+import random, math
 from sklearn.linear_model import LogisticRegression, SGDRegressor, Lasso, ElasticNet
 from sklearn.ensemble import GradientBoostingRegressor
+from sklearn import preprocessing
 import datetime
 
-
-DATA_PATH = "./data/drivers/"
+start = datetime.datetime.now()
+# DATA_PATH = "./data/drivers/"
+DATA_PATH = "./"
 
 def parse_data(file):
     data = []
@@ -22,7 +24,7 @@ def parse_data(file):
 #From SciPy cookbook
 def savitzky_golay(y, window_size, order, deriv=0, rate=1):
     r"""Smooth (and optionally differentiate) data with a Savitzky-Golay filter.
-    The Savitzky-Golay filter removes high frequency noise from data.
+    The Savitzky-Golay filwter removes high frequency noise from data.
     It has the advantage of preserving the original shape and
     features of the signal better than other types of filtering
     approaches, such as moving averages techniques.
@@ -110,11 +112,45 @@ def get_distance(data):
     end = data[-1]
     return [norm(start-end)]
 
+def getAngles(data):
+    # Helper function to get the turning angles for a driver's journey.
+    v = 0;
+    angles = []
+    while v < len(data[:-2]):
+        p1, p2, p3 = data[v], data[v+1], data[v+2];
+        # p2 is starting point (or corner) of both vectors
+        v1, v2 = p1 - p2, p3 - p2;
+        
+        # if no change in position, move on
+        if (sum(v1) == 0 or sum(v2) == 0):
+            v += 2
+            continue
+            
+        # calculate angle from dot product
+        v1, v2 = v1 / sum(np.abs(v1)), v2 / sum(np.abs(v2)) # normalize
+        m1, m2 = np.linalg.norm(v1), np.linalg.norm(v2); # magnitudes
+        frac = (np.dot(v1, v2)) / (m1 * m2)
+        if (frac > 1): frac = 1.0 # for underflow
+        elif (frac < -1): frac = -1
+            
+        angle = math.acos(frac) * (180./math.pi) # degrees
+        if (angle > 360):
+            angle -= 360
+
+        angles.append(angle)
+        v += 1
+    return angles
+
+def get_turnMetrics(data):
+    # Return mean and standard deviation of the turn angles.
+    angles = getAngles(data)
+    return [sum(angles) / len(angles), np.std(angles)]
+
 def get_features(data):
-    # Smooth the data
     x_vals = zip(*data)[0]
     y_vals = zip(*data)[1]
 
+    # smooth the data
     x_smooth = savitzky_golay(x_vals, 29, 3)
     y_smooth = savitzky_golay(y_vals, 29, 3)
 
@@ -128,17 +164,23 @@ def get_features(data):
 
     data = np.asarray(smoothed_data)
 
-    # Get the three features
+    # get the features; speed, length, distance
     speed_features = get_speed_features(data)
     length = get_length(data)
     dist = get_distance(data)
-    features = speed_features + length + dist
+    turnFreq = get_turnMetrics(data)
+    features = speed_features + length + dist + turnFreq
     return features
 
 def get_driver_list():
     driver_list = []
     for item in listdir(DATA_PATH):
-        driver_list.append(int(item))
+        # two lines below just a peculiarity for my own system's filepath
+        if not item[0].isdigit():
+            continue
+
+        if not (item == ".DS_Store"):
+            driver_list.append(int(item))
     return driver_list
 
 #Generates training set.  All data associated with a given driver is assumed to be from that driver.
@@ -152,6 +194,7 @@ def get_training_set(driver_number, num_false):
     for file in listdir(path):
         data = parse_data(path+file)
         features = get_features(data)
+        #features = [len(data)]
         training_set.append(features)
         training_output.append(1)
 
@@ -161,11 +204,13 @@ def get_training_set(driver_number, num_false):
     ctr = 0
     while ctr < num_false:
         false_driver_num = random.choice(driver_list)
+        print "FD"+str(false_driver_num)
         false_path = DATA_PATH + str(false_driver_num) + "/"
 
         for file in listdir(false_path):
-            false_data = parse_data(path+file)
-            false_features = get_features(data)
+            false_data = parse_data(false_path+file)
+            false_features = get_features(false_data)
+            #false_features = [len(false_data)]
             training_set.append(false_features)
             training_output.append(0)
 
@@ -175,32 +220,48 @@ def get_training_set(driver_number, num_false):
     return training_set, training_output
 
 def generate_submission_file(num_false):
-    # driver_list = get_driver_list()
+    driver_list = get_driver_list()
+    print len(driver_list)
+    print num_false
     predictions = {}
-    driver_list = [1, 2, 3]
+    # driver_list = [1, 2, 3] # for fewer drivers
     for driver_number in driver_list:
-        print "Training on:"+str(driver_number)
+        print "GB training on:"+str(driver_number)
         ts = get_training_set(driver_number,num_false)
+        f = open('ts.txt','wb')
+        f.write(str(ts))
 
-        #model = GradientBoostingRegressor()
-        model = LogisticRegression()
+        '''
+        path = DATA_PATH+str(driver_number) + "/"
+        for file in listdir(path):
+            data = parse_data(path+file)
+            features = get_features(data)
+            training_set.append(features)
+            training_output.append(1)
+        '''
+
+        model = GradientBoostingRegressor()
+        #model = LogisticRegression()
         #model = SGDRegressor()
         #model = Lasso()
         #model = ElasticNet()
+        #ts_scaled = (preprocessing.scale(ts[0]), ts[1])
         model.fit(ts[0], ts[1])
         predictions[str(driver_number)] = (model.predict(ts[0][:200]))
 
-    print predictions
-    # with open('submission.csv', 'w') as csvfile:
-    #     fieldnames = ['driver_trip', 'prob']
-    #     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-    #     writer.writeheader()
+    # print predictions
+    with open('submission_en.csv', 'w') as csvfile:
+        fieldnames = ['driver_trip', 'prob']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        writer.writeheader()
 
-    #     for driver_number in driver_list:
-    #         print "Writing:"+str(driver_number)
-    #         ctr = 0
-    #         while ctr < 200:
-    #             writer.writerow({'driver_trip': str(driver_number)+'_'+str(ctr+1), 'prob':predictions[str(driver_number)][ctr]})
-    #             ctr += 1
+        for driver_number in driver_list:
+            print "Writing:"+str(driver_number)
+            ctr = 0
+            while ctr < 200:
+                writer.writerow({'driver_trip': str(driver_number)+'_'+str(ctr+1), 'prob':predictions[str(driver_number)][ctr]})
+                ctr += 1
 
-generate_submission_file(1)
+# generate_submission_file(5)
+# print datetime.datetime.now() - start
+
